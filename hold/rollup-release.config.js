2997168d659ab522b60a7156669a53f1424fce5e
Tim McMaster <tim@mcmaster.id.au>
2019-09-18 10:58:39 +1000

    removed the hold directory

--- hold/rollup-release.config.js
+++
@@ -1,9 +0,0 @@
-import createDefaultConfig from '@open-wc/building-rollup/modern-config';
-
-// noinspection JSUnusedGlobalSymbols
-export default createDefaultConfig(
-    {
-        input: 'src/index.js',
-        outputDir: 'dist/release'
-    }
-);
